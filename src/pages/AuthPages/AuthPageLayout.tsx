cc3d9e4bac2c8989fe1fb292cf518f5caf874ab2
anikavu <cuongmc1404@gmail.com>
2025-06-08 23:25:47 +0700

    bug-fix: search , validate, syntax, ui medical service

--- src/pages/AuthPages/AuthPageLayout.tsx
+++ src/pages/AuthPages/AuthPageLayout.tsx
@@ -1,8 +1,6 @@
 import React from "react";
-import GridShape from "../../components/common/GridShape";
 import { Link } from "react-router";
 import ThemeTogglerTwo from "../../components/common/ThemeTogglerTwo";
-
 export default function AuthLayout({
   children,
 }: {
@@ -13,24 +11,15 @@ export default function AuthLayout({
       <div className="relative flex flex-col justify-center w-full h-screen lg:flex-row dark:bg-gray-900 sm:p-0">
         {children}
         <div className="items-center hidden w-full h-full lg:w-1/2 bg-brand-950 dark:bg-white/5 lg:grid">
-          <div className="relative flex items-center justify-center z-1">
-            {/* <!-- ===== Common Grid Shape Start ===== --> */}
-            <GridShape />
-            <div className="flex flex-col items-center max-w-xs">
-              <Link to="/" className="block mb-4">
-                <img
-                  width={231}
-                  height={48}
-                  src="/images/logo/auth-logo.svg"
-                  alt="Logo"
-                />
-              </Link>
-              <p className="text-center text-gray-400 dark:text-white/60">
-                Free and Open-Source Tailwind CSS Admin Dashboard Template
-              </p>
-            </div>
+          <div className="relative flex items-center justify-center w-full h-full z-1">
+            <img
+              src="/images/19778.jpg"
+              alt="Auth Background"
+              className="object-cover h-full w-full max-h-screen bg-gray-500"
+            />
           </div>
         </div>
+
         <div className="fixed z-50 hidden bottom-6 right-6 sm:block">
           <ThemeTogglerTwo />
         </div>
--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -11,7 +11,7 @@ axiosInstance.interceptors.request.use(
   (config) => {
     // const token = localStorage.getItem('authToken');
     const token =
-      "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2bi5lZHUuZnB0Lm1lZGljYWxkaWFnbm9zaXMiLCJzdWIiOiJhZG1pbiIsImV4cCI6MTc0OTM3NTYyMywiaWF0IjoxNzQ5MzcyMDIzLCJqdGkiOiJhZmE2ODU1Mi1hODJlLTRjZjUtYjE4Yi02NjhhNmE2NjIxZGMiLCJzY29wZSI6IlJPTEVfQURNSU4ifQ.el33TjkbWR5rKnQrVNwYUXGSMirNRkNel_kfoh1Ue2iHgkAn_D5hxf3yNLVORDNSJ8_1DZbHFdklCe32wYC1xQ";
+      "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2bi5lZHUuZnB0Lm1lZGljYWxkaWFnbm9zaXMiLCJzdWIiOiJhZG1pbiIsImV4cCI6MTc0OTQwMjE0NywiaWF0IjoxNzQ5Mzk4NTQ3LCJqdGkiOiJlM2ZhNTk2Zi1kM2RhLTQ4YmMtYTA4MC1lOTUwOTljNjk1N2QiLCJzY29wZSI6IlJPTEVfQURNSU4ifQ.5NoFqD5UuYLXIa-ma8qflRGQanrfFn9VbOp3Edclrb_k-ADtV43hLP7Pkx4NbJPmflJcsW2kWWq8V_LxTYV-OQ";
     // const hostname = window.location.hostname;
     // const subdomain = hostname.split('.')[0];  // Lấy phần subdomain (ví dụ: hospital_abc)
     const subdomain = "hadong";
