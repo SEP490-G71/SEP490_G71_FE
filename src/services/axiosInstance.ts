679e3a6e867010c4a7ea42602498bce79bea68c9
Nguyen Dac Dat <125476394+datnd10@users.noreply.github.com>
2025-06-08 23:30:08 +0700

    Update axiosInstance.ts

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -10,8 +10,7 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     // const token = localStorage.getItem('authToken');
-    const token =
-      "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2bi5lZHUuZnB0Lm1lZGljYWxkaWFnbm9zaXMiLCJzdWIiOiJhZG1pbiIsImV4cCI6MTc0OTQwMjE0NywiaWF0IjoxNzQ5Mzk4NTQ3LCJqdGkiOiJlM2ZhNTk2Zi1kM2RhLTQ4YmMtYTA4MC1lOTUwOTljNjk1N2QiLCJzY29wZSI6IlJPTEVfQURNSU4ifQ.5NoFqD5UuYLXIa-ma8qflRGQanrfFn9VbOp3Edclrb_k-ADtV43hLP7Pkx4NbJPmflJcsW2kWWq8V_LxTYV-OQ";
+    const token = "";
     // const hostname = window.location.hostname;
     // const subdomain = hostname.split('.')[0];  // Lấy phần subdomain (ví dụ: hospital_abc)
     const subdomain = "hadong";
