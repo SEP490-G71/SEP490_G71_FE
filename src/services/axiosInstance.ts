7889d19e9ba5d68407c1c82df6192e6e393ed4c9
anikavu <cuongmc1404@gmail.com>
2025-06-08 23:45:17 +0700

    bug-fix: validate textInput price in modal medical service

--- src/services/axiosInstance.ts
+++ src/services/axiosInstance.ts
@@ -10,7 +10,8 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(
   (config) => {
     // const token = localStorage.getItem('authToken');
-    const token = "";
+    const token =
+      "eyJhbGciOiJIUzUxMiJ9.eyJpc3MiOiJ2bi5lZHUuZnB0Lm1lZGljYWxkaWFnbm9zaXMiLCJzdWIiOiJhZG1pbiIsImV4cCI6MTc0OTQwNDEzMywiaWF0IjoxNzQ5NDAwNTMzLCJqdGkiOiIxMTk2NWNkZS1mMDc2LTQ5MjMtODdlYi01NWMwYmYxZmI4ZTQiLCJzY29wZSI6IlJPTEVfQURNSU4ifQ.djsY1k6LdHrLeaN0d7NcXjwFdIZR_DEBs0V8opUdmq9Dlu0BRnU9PjuK6SSt_AfslCYYVAZg7CYfIEp54yYyAA";
     // const hostname = window.location.hostname;
     // const subdomain = hostname.split('.')[0];  // Lấy phần subdomain (ví dụ: hospital_abc)
     const subdomain = "hadong";
